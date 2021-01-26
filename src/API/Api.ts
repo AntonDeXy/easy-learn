a96fd4a4b987e854a672a545a045461bd3be4d06
AntonDeXy <podolyananton@gmail.com>
2021-01-26 20:56:10 +0200

    get baseUrl from .env

--- src/API/Api.ts
+++ src/API/Api.ts
@@ -4,11 +4,7 @@ import { NoteType } from '../redux/reducers/notes/notesReducer'
 import { UserQuestionType } from '../redux/reducers/users/usersReducer'
 import { HelpItemType } from '../redux/reducers/help/helpPageReducer'
 
-export const baseURL = 'http://localhost:5001/api/v1/'
-// export const baseURL = 'https://cors-anywhere.herokuapp.com/https://dexy.site/api/v1/'
-// export const baseURL = '/api/v1/'
-// export const baseURL = 'https://easy-lern-backend.herokuapp.com/api/v1/'
-
+const baseURL = process.env.REACT_APP_BASE_API_URL || 'http://localhost:5001/api/v1/'
 
 const instance = axios.create({
   baseURL
